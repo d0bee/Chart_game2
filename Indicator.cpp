e1c7aaa02d6bdce76c7b10cec4a4607ff881919f
d0bee <gusdud9745@gmail.com>
2023-09-02 21:14:23 +0900

    feat : Indicator cpp/h 추가

---
+++ Indicator.cpp
@@ -0,0 +1,34 @@
+﻿// Indicator.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ChartGame.h"
+#include "afxdialogex.h"
+#include "Indicator.h"
+
+
+// Indicator 대화 상자
+
+IMPLEMENT_DYNAMIC(Indicator, CDialogEx)
+
+Indicator::Indicator(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CHARTGAME_INDICATOR, pParent)
+{
+
+}
+
+Indicator::~Indicator()
+{
+}
+
+void Indicator::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(Indicator, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// Indicator 메시지 처리기
---
+++ Indicator.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// Indicator 대화 상자
+
+class Indicator : public CDialogEx
+{
+	DECLARE_DYNAMIC(Indicator)
+
+public:
+	Indicator(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~Indicator();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CHARTGAME_INDICATOR };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+};
